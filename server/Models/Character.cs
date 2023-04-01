faf66e9de8dc3292504daef6a7dd0f25fda9a517
Solomon Lambert <sol.lambert@gmail.com>
2023-04-01 12:59:56 -0500

    initial commit

---
+++ server/Models/Character.cs
@@ -0,0 +1,9 @@
+namespace dnd_weekend_project.Models;
+
+public class Character {
+
+    public enum Race {HUMAN, ELF, DWARF, GNOME, HALFLING, HALFORC, HALFELF}
+
+    public enum Profession {BARD, CLERIC, DRUID, FIGHTER, RANGER, WIZARD, SORCEROR, PALADIN}
+
+}
