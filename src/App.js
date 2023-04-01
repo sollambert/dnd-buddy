02449164c7d9383314c893c3f9822631a091ae05
Solomon Lambert <sol.lambert@gmail.com>
2023-04-01 18:14:24 -0500

    added inputs for all character stats

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import CharacterForm from './Components/CharCreator/CharacterForm.tsx';
+import logo from "./logo.svg";
+import "./App.css";
+import CharacterForm from "./Components/CharCreator/CharacterForm.tsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <CharacterForm/>
-        </a>
-      </header>
+        <CharacterForm />
     </div>
   );
 }
--- src/Components/CharCreator/CharacterForm.tsx
+++ src/Components/CharCreator/CharacterForm.tsx
@@ -1,52 +1,152 @@
-import React, { useState, useEffect, ReactEventHandler } from "react"
+import React, { useState, useEffect, ReactEventHandler } from "react";
 
-type AppProps = {
-}
+type AppProps = {};
 
 class InputBuffer {
-    name: string;
-    level: number;
-    race: string;
-    profession: string;
-    str: number;
-    dex: number;
-    con: number;
-    int: number;
-    wis: number;
-    cha: number;
-    [key: string]: any;
+  name: string;
+  level: number;
+  race: string;
+  profession: string;
+  str: number;
+  dex: number;
+  con: number;
+  int: number;
+  wis: number;
+  cha: number;
+  [key: string]: any;
 
-    constructor(name: string, level: number, race: string, profession: string, str: number, dex: number, con: number, int: number, wis: number, cha: number) {
-        this.name = name;
-        this.level = level;
-        this.race = race;
-        this.profession = profession;
-        this.str = str;
-        this.dex = dex;
-        this.con = con;
-        this.int = int;
-        this.wis = wis;
-        this.cha = cha;
-    }
+  constructor(
+    name: string,
+    level: number,
+    race: string,
+    profession: string,
+    str: number,
+    dex: number,
+    con: number,
+    int: number,
+    wis: number,
+    cha: number
+  ) {
+    this.name = name;
+    this.level = level;
+    this.race = race;
+    this.profession = profession;
+    this.str = str;
+    this.dex = dex;
+    this.con = con;
+    this.int = int;
+    this.wis = wis;
+    this.cha = cha;
+  }
 }
 
-function CharacterForm({} : AppProps) : JSX.Element {
+function CharacterForm({}: AppProps): JSX.Element {
+  const [inputBuffer, setInputBuffer] = useState<InputBuffer>(
+    new InputBuffer("", 1, "", "", 0, 0, 0, 0, 0, 0)
+  );
 
+  const [result, setResult] = useState<string>("");
 
-    const [inputBuffer, setInputBuffer] = useState<InputBuffer>(new InputBuffer("", 0, "", "", 0, 0, 0, 0, 0, 0)); 
-
-    function handleInput (event: any, key: string) {
-        const inputKey = key as keyof typeof inputBuffer;
-        setInputBuffer({...inputBuffer, [inputKey]: event.target.value});
+  function handleInput(event: any, key: string) {
+    if (key == "level") {
+        event.target.value = Math.max(1, Math.min(event.target.value, 20));
     }
+    const inputKey = key as keyof typeof inputBuffer;
+    setInputBuffer({ ...inputBuffer, [inputKey]: event.target.value });
+  }
 
-    return (
-        <>
-            <h1>I'm a typescript component</h1>
-            <label htmlFor="name">Name: </label>
-            <input name="name" type="text" onChange={(e) => handleInput(e, "name")} value={inputBuffer.name}></input>
-        </>
-    )
+  return (
+    <>
+      <h1>I'm a typescript component</h1>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          name="name"
+          type="text"
+          onChange={(e) => handleInput(e, "name")}
+          value={inputBuffer.name}
+          placeholder="Enter name..."
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="name">Race: </label>
+        <input
+          name="race"
+          type="text"
+          onChange={(e) => handleInput(e, "race")}
+          value={inputBuffer.race}
+          placeholder="Enter race..."
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="class">Class: </label>
+        <input
+          name="class"
+          type="text"
+          onChange={(e) => handleInput(e, "profession")}
+          value={inputBuffer.profession}
+          placeholder="Enter class..."
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="level">Level: </label>
+        <input
+          name="level"
+          type="number"
+          min="1"
+          max="20"
+          onChange={(e) => handleInput(e, "level")}
+          value={inputBuffer.level}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="str">Strength: </label>
+        <input
+          name="str"
+          type="number"
+          onChange={(e) => handleInput(e, "str")}
+          value={inputBuffer.str}
+        ></input>
+        <label htmlFor="dex">Dexterity: </label>
+        <input
+          name="dex"
+          type="number"
+          onChange={(e) => handleInput(e, "dex")}
+          value={inputBuffer.dex}
+        ></input>
+        <label htmlFor="con">Constitution: </label>
+        <input
+          name="con"
+          type="number"
+          onChange={(e) => handleInput(e, "con")}
+          value={inputBuffer.con}
+        ></input>
+        <label htmlFor="int">Intelligence: </label>
+        <input
+          name="int"
+          type="number"
+          onChange={(e) => handleInput(e, "int")}
+          value={inputBuffer.int}
+        ></input>
+        <label htmlFor="wis">Wisdom: </label>
+        <input
+          name="wis"
+          type="number"
+          onChange={(e) => handleInput(e, "wis")}
+          value={inputBuffer.wis}
+        ></input>
+        <label htmlFor="cha">Charisma: </label>
+        <input
+          name="cha"
+          type="number"
+          onChange={(e) => handleInput(e, "cha")}
+          value={inputBuffer.cha}
+        ></input>
+      </div>
+      <button onClick={() => {setResult(JSON.stringify(inputBuffer))}}>SHOW</button>
+      {result}
+    </>
+  );
 }
 
 export default CharacterForm;
