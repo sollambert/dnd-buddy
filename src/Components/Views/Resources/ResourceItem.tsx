5ceb344357f8fc9adee15954019dcdc93bec7eaf
Solomon Lambert <sol.lambert@gmail.com>
2023-05-09 13:55:53 -0500

    add ctrl click to copy resource item url

--- src/Components/Views/Resources/ResourceItem.tsx
+++ src/Components/Views/Resources/ResourceItem.tsx
@@ -6,20 +6,32 @@ type Props = {
   result: ApiResult;
 };
 
+const handleMouseDown = (e: any, result: ApiResult, history?: any) => {
+  if (e.button === 0) {
+    if (e.ctrlKey) {
+      navigator.clipboard.writeText(result.url.replace('/api/', '/resources/'))
+        .then(() => alert('Copied!'));
+    } else {
+      history?.push(result.url.replace('/api/', '/resources/'));
+    }
+  }
+  else if (e.button === 1) {
+    console.log(e);
+    window.open(result.url.replace('/api/', '/resources/'));
+  } else if (e.button === 2) {
+  }
+}
+
 function ResourceItem({ result }: Props): JSX.Element {
   const history = useHistory();
 
   return (
-    <>
-      <div className="resource-redirect" style={{ border: "1px solid #000000", padding: ".25em" }} onAuxClick={(e) => {
-        if (e.button === 1) {
-          window.open(result.url.replace('/api/', '/resources/'));
-        }
-      }}
-        onClick={() => {
-          history.push(result.url.replace('/api/', '/resources/'));
-        }}>{result.name}</div>
-    </>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="resource-redirect" style={{ border: "1px solid #000000", padding: ".25em" }} onMouseDown={(e) => {
+        handleMouseDown(e, result, history);}}>
+        {result.name}
+      </div>
+    </div>
   );
 }
 
